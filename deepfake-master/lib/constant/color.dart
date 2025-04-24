fbdc3628bcdef543b528725057bc430f4dc21a0d
mohammed <zxzxmop5@gmail.com>
2025-04-24 13:31:05 +0200

    delete not neccery folder

--- deepfake-master/lib/constant/color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorConst {
-  static Color base = Color(0xffc2888d);
-  static Color second = Color(0xff524973);
-  static Color white = Colors.white;
-}
